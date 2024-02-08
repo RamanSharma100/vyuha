0af96609ace01ab887f7486463b6e0b938252d81
Raman Sharma <techraman100@gmail.com>
2024-02-08 21:36:53 +0530

    Feat: base template support

--- examples/index.ts
+++ examples/index.ts
@@ -1,21 +1,52 @@
 import Vyuha from '../src/Vyuha';
 
-const html = `
+const baseTemplate = `
 <!DOCTYPE html>
 <html>
     <head>
-        <title>{{ title }}</title>
+        <title>
+            @block title @endblock
+        </title>
     </head>
     <body>
-        <p>Welcome {{ name }}</p>
+        <h1>{{ title }}</h1>
+        @block content @endblock
     </body>
 </html>
 `;
 
-const template = new Vyuha(html);
+const extendedHtml = `
+@extends "base"
+
+@block title
+    {{ title }}
+@endblock
+
+@block content
+    <h1>Hello, {{ name }}</h1>
+@endblock
+`;
+
+const html = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>
+                {{ title }}
+            </title>
+        </head>
+        <body>
+            <h1>Hello, {{ name }}</h1>
+        </body>
+    </html>
+
+`;
+
+const template = new Vyuha(extendedHtml, baseTemplate);
 
 const rendered = template.render({
 	title: 'Hello World',
 	name: 'Raman Sharma',
+	test: 'test',
 });
 console.log(rendered);
